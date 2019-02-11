6d3e1db975948a59b718476c1c3979ad147da6fb
PLPANIE <pawel.niesciur@gmail.com>
2019-02-11 17:42:28 +0100

    RecyclerView Adapter in progress

---
+++ app/src/main/java/com/niesciur/pawel/remindlyv1/NoteRecyclerAdapter.java
@@ -0,0 +1,15 @@
+package com.niesciur.pawel.remindlyv1;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class NoteRecyclerAdapter {
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
